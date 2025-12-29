14c0f03478c0acedb72f5650112accccf756b557
Anuhk <145833081+Anuhk@users.noreply.github.com>
2025-12-29 17:05:54 +0530

    Trained model for classification

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets,layers,models
+from tensorflow.keras.models import load_model
 
 
 
@@ -12,12 +13,42 @@ training_img,testing_img=training_img/255,testing_img/255
 
 class_names=['Plane','Car','Bird','cat','Deer','Dog','Frog','Horse','Ship','Truck']
 
-for i in range(16):
-    plt.subplot(4,4,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.imshow(training_img[i],cmap=plt.cm.binary)
-    plt.xlabel(class_names[training_labels[i][0]])
+# for i in range(16):
+#     plt.subplot(4,4,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.imshow(training_img[i],cmap=plt.cm.binary)
+#     plt.xlabel(class_names[training_labels[i][0]])
 
-plt.show()
+# plt.show()
+
+#Taking small subset of data to reduce training time
+training_img=training_img[:20000]
+training_labels=training_labels[:20000]
+testing_img=testing_img[:4000]
+testing_labels=testing_labels[:4000]
+
+
+model=models.Sequential()
+model.add(layers.Conv2D(32,(3,3), activation='relu',input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.MaxPooling2D(2,2))
+model.add(layers.Conv2D(64,(3,3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(10,activation='softmax'))
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(training_img,training_labels, epochs=10, validation_data=(testing_img,testing_labels))
+
+loss,accuracy=model.evaluate(testing_img,testing_labels)
+
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+model.save('image_classifier.keras')
+
+
 
